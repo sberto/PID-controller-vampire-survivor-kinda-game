62c9afd55d63fc78213af4970fb58813806213c8
sberto <s.bertolotto92@gmail.com>
2024-10-12 23:53:11 +0200

    add standard projectile

---
+++ linear_projectile.gd
@@ -0,0 +1,4 @@
+extends CharacterBody2D
+
+func _process(delta: float) -> void:
+	move_and_slide()
--- mouse.gd
+++ mouse.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 var vulnerable: bool = true
+var velocity: Vector2
+@onready var l_proj: CharacterBody2D = $"../LinearProjectile"
 
 @onready var max_health = health
 var health = 10:
@@ -14,7 +16,10 @@ var health = 10:
 			
 
 func _physics_process(delta: float) -> void:
+	var old_position = position
 	position = get_viewport().get_mouse_position()
+	if (position - old_position).length() > 0:
+		velocity = position - old_position
 
 func _ready() -> void:
 	body_entered.connect(_hit)
@@ -26,3 +31,6 @@ func _hit(body: CharacterBody2D):
 	vulnerable = false
 	get_tree().create_timer(1).timeout.connect(func(): vulnerable = true)
 	
+func _on_shoot_timeout() -> void:
+	l_proj.position = position
+	l_proj.velocity = velocity.normalized() * 1000
